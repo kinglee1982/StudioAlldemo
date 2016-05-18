f68452dfc94c08fc8fceb0222e0b60180df9ff1e
wangbs <563492052@qq.com>
2016-05-18 16:02:10 +0800

    新增

--- app/src/main/java/com/app/alldemo/effect/GildActivity.java
+++ app/src/main/java/com/app/alldemo/effect/GildActivity.java
@@ -17,6 +17,7 @@ public class GildActivity extends Activity {
         image=(ImageView)findViewById(R.id.image);
         String url="http://52.192.66.23/data/timages/39/39_0.jpg?1454086351";
         Glide.with(this).load(Uri.parse(url)).centerCrop().into(image);
+//        Glide.with(this).load(Uri.parse(url)).placeholder(new ColorDrawable(Color.WHITE)).diskCacheStrategy(DiskCacheStrategy.ALL).into(image);
     }
     private void findUI(){
     }
