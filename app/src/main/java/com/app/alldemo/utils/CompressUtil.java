ea0647581b43b77e1a75d32f5abf6e3e5b2b053c
wangbs <563492052@qq.com>
2016-08-04 13:33:58 +0800

    收到

--- app/src/main/java/com/app/alldemo/utils/CompressUtil.java
+++ app/src/main/java/com/app/alldemo/utils/CompressUtil.java
@@ -10,115 +10,108 @@ import java.io.IOException;
 
 /**
  * Created by wangbs on 16/8/3.
- * 压缩
+ * 压缩图片
  */
 public class CompressUtil {
-    private static final int maxNum=100;
-    public static byte[] compressImageFromFile(byte[] frontBytes,String srcPath) {
-        if(frontBytes.length/1024>maxNum){
-            BitmapFactory.Options newOpts = new BitmapFactory.Options();
-            newOpts.inJustDecodeBounds = true;//只读边,不读内容
-            Bitmap bitmap = BitmapFactory.decodeFile(srcPath, newOpts);
-            newOpts.inJustDecodeBounds = false;
-            int w = newOpts.outWidth;
-            int h = newOpts.outHeight;
-            float hh = 800f;
-            float ww = 480f;
-            int be = 1;
-            if (w > h && w > ww) {
-                be = (int) (newOpts.outWidth / ww);
-            } else if (w < h && h > hh) {
-                be = (int) (newOpts.outHeight / hh);
-            }
-            if (be <= 0)
-                be = 1;
-            newOpts.inSampleSize = be;//设置采样率
-            newOpts.inPreferredConfig = Bitmap.Config.ARGB_8888;//该模式是默认的,可不设
-            newOpts.inPurgeable = true;// 同时设置才会有效
-            newOpts.inInputShareable = true;//。当系统内存不够时候图片自动被回收
-            bitmap = BitmapFactory.decodeFile(srcPath, newOpts);
-            int roteValue=roteValue(srcPath);
-            if(roteValue!=0){
-                Matrix matrix = new Matrix();
-                matrix.setRotate(roteValue);
-                bitmap = Bitmap.createBitmap(bitmap, 0, 0,
-                        bitmap.getWidth(), bitmap.getHeight(), matrix, true);
-            }
-            return getBytes(bitmap,frontBytes);
-        }else{
-            return frontBytes;
+    private static final String TAG="CompressUtil";
+    private static final int maxNum = 100;//kb
+    private static CompressUtil instance;
+    public static CompressUtil getInstance(){
+        if(instance == null){
+            instance = new CompressUtil();
         }
+        return instance;
     }
-    private static int roteValue(String srcPath){
-        int roteValue=0;
-        int orientation = ExifInterface.ORIENTATION_UNDEFINED;
+
+    /**
+     *
+     * @param srcPath
+     * 说明:
+     * Bitmap.Config ARGB_4444：每个像素占四位，即A=4，R=4，G=4，B=4，那么一个像素点占4+4+4+4=16位
+     * Bitmap.Config ARGB_8888：每个像素占四位，即A=8，R=8，G=8，B=8，那么一个像素点占8+8+8+8=32位
+     * Bitmap.Config RGB_565：每个像素占四位，即R=5，G=6，B=5，没有透明度，那么一个像素点占5+6+5=16位
+     * Bitmap.Config ALPHA_8：每个像素占四位，只有透明度，没有颜色。
+     * 一般情况下我们都是使用的ARGB_8888，由此可知它是最占内存的，因为一个像素占32位，8位=1字节，所以一个像素占4字节的内存。假设有一张480x800的图片，如果格式为ARGB_8888，那么将会占用1500KB的内存。
+     * @return
+     */
+    public byte[] compressImageFromFile(String srcPath) {
+        BitmapFactory.Options newOpts = new BitmapFactory.Options();
+        newOpts.inJustDecodeBounds = true;//只读边,不读内容
+        BitmapFactory.decodeFile(srcPath, newOpts);
+        newOpts.inJustDecodeBounds = false;
+        int newOptsWidth = newOpts.outWidth;
+        int newOptsHeight = newOpts.outHeight;
+        newOpts.inSampleSize = getSampleSize(newOptsWidth,newOptsHeight);//设置缩放比例
+        newOpts.inPreferredConfig = Bitmap.Config.RGB_565;
+        newOpts.inPurgeable = true;// 同时设置才会有效
+        newOpts.inInputShareable = true;//。当系统内存不够时候图片自动被回收
+        Bitmap bitmap = BitmapFactory.decodeFile(srcPath, newOpts);
+        int roteValue = roteValue(srcPath);
+        if (roteValue != 0) {
+            Matrix matrix = new Matrix();
+            matrix.setRotate(roteValue);
+            bitmap = Bitmap.createBitmap(bitmap, 0, 0,
+                    bitmap.getWidth(), bitmap.getHeight(), matrix, true);
+        }
+        return getBytes(bitmap);
+    }
+    private static int getSampleSize(int newOptsWidth,int newOptsHeight){
+        double scale = Math.sqrt((newOptsWidth * newOptsHeight)/(800.0*480.0));
+        int scaleValue= scale< 1 ? 1 : (int)scale;
+        MyLog.e(TAG,"scaleValue:"+scaleValue);
+        return scaleValue;
+    }
+
+    private int roteValue(String srcPath) {
+        int roteValue = 0;
         try {
             ExifInterface exifInterface = new ExifInterface(srcPath);
-            orientation = exifInterface.getAttributeInt(
+            int orientation = exifInterface.getAttributeInt(
                     ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_UNDEFINED);
             // 是否旋转图片
-            if(orientation != ExifInterface.ORIENTATION_NORMAL
+            if (orientation != ExifInterface.ORIENTATION_NORMAL
                     && orientation != ExifInterface.ORIENTATION_UNDEFINED) {
                 switch (orientation) {
                     case ExifInterface.ORIENTATION_FLIP_HORIZONTAL:
-                        roteValue=0;
+                        roteValue = 0;
                         break;
 
                     case ExifInterface.ORIENTATION_FLIP_VERTICAL:
-                        roteValue=0;
+                        roteValue = 0;
                         break;
 
                     case ExifInterface.ORIENTATION_ROTATE_90:
-                        roteValue=90;
+                        roteValue = 90;
                         break;
 
                     case ExifInterface.ORIENTATION_ROTATE_180:
-                        roteValue=180;
+                        roteValue = 180;
                         break;
 
                     case ExifInterface.ORIENTATION_ROTATE_270:
-                        roteValue=270;
+                        roteValue = 270;
                         break;
                 }
             }
-        }
-        catch(IOException e) {
+        } catch (IOException e) {
             e.printStackTrace();
         }
         return roteValue;
     }
-    private static byte[] getBytes(Bitmap bitmap,byte[] frontBytes){
-        ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        bitmap.compress(Bitmap.CompressFormat.PNG, 100, baos);
-        if(baos.toByteArray().length<frontBytes.length){
-            if(baos.toByteArray().length/1024>maxNum){
-                int options = 100;
-                while (baos.toByteArray().length / 1024 > maxNum) {
-                    baos.reset();
-                    options -= 10;
-                    bitmap.compress(Bitmap.CompressFormat.JPEG, options, baos);
-                }
-                MyLog.e("compressImageFromFile","11111压缩后的大小:"+baos.toByteArray().length+"压缩前:"+frontBytes.length);
-                return baos.toByteArray();
-            }else{
-                MyLog.e("compressImageFromFile","2222压缩后的大小:"+baos.toByteArray().length+"压缩前:"+frontBytes.length);
-                return baos.toByteArray();
-            }
-        }else{
-            byte[] front2Bytes=compressBmpToFile(BitmapFactory.decodeByteArray(frontBytes, 0, frontBytes.length));
-            MyLog.e("compressImageFromFile","33333压缩后的大小:"+front2Bytes.length+"压缩前:"+frontBytes.length);
-            return front2Bytes;
-        }
-    }
-    public static byte[] compressBmpToFile(Bitmap image){
-        ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        image.compress(Bitmap.CompressFormat.JPEG, 100, baos);//质量压缩方法，这里100表示不压缩，把压缩后的数据存放到baos中
+
+    private byte[] getBytes(Bitmap bitmap) {
         int options = 100;
-        while ( baos.toByteArray().length / 1024>maxNum) { //循环判断如果压缩后图片是否大于100kb,大于继续压缩
-            baos.reset();//重置baos即清空baos
-            options -= 10;//每次都减少10
-            image.compress(Bitmap.CompressFormat.JPEG, options, baos);//这里压缩options%，把压缩后的数据存放到baos中
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.PNG, options, baos);
+        if (baos.toByteArray().length / 1024 > maxNum) {
+            while (baos.toByteArray().length / 1024 > maxNum) {
+                baos.reset();
+                options -= 10;
+                bitmap.compress(Bitmap.CompressFormat.JPEG, options, baos);
+            }
+            return baos.toByteArray();
+        } else {
+            return baos.toByteArray();
         }
-        return baos.toByteArray();
     }
 }
